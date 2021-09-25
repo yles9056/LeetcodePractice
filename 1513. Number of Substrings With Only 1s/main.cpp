1a8419e03564841486599c231a3314eb23d18a87
Jonathan Tsai <jonathantsai@wistronits.com>
2021-09-25 14:26:08 +0800

    Add 230. Kth Smallest Element in a BST/main.cpp(complete) Change Cpp formatter settings.

--- 1513. Number of Substrings With Only 1s/main.cpp
+++ 1513. Number of Substrings With Only 1s/main.cpp
@@ -1,22 +1,16 @@
-class Solution
-{
-public:
-  int numSub(string s)
-  {
+class Solution {
+ public:
+  int numSub(string s) {
     int currentCount = 0;
     int ans = 0;
     int mod = 1e9 + 7;
-    for (char c : s)
-    {
+    for (char c : s) {
       // Alternatively, use: (1 + currentCount) * currentCount / 2
-      if (c == '1')
-      {
+      if (c == '1') {
         currentCount += 1;
         ans += currentCount;
         ans %= mod;
-      }
-      else
-      {
+      } else {
         currentCount = 0;
       }
     }
